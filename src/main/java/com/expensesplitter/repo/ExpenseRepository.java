8a89265b780bcddac3df8ec558032014be7a02ba
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-06-25 13:09:06 +0530

    Update ExpenseRepository.java

--- src/main/java/com/expensesplitter/repo/ExpenseRepository.java
+++ src/main/java/com/expensesplitter/repo/ExpenseRepository.java
@@ -7,7 +7,17 @@ import com.expensesplitter.entity.Expense;
 
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-	// write custom query to fetch list of expenses by user id
+	//  write your code to fetch all expenses paid by a specific user and it must return data in list.
 
-	// dynamic query to find net balance between 2 users
+
+	//  write your code to fetch all expenses where a specific user is part of the sharedWith list.
+
+
+	//  write a query to find all expenses involving two users, either as payer or as a participant in the shared list.
+
+	
+	//  write custom query to fetch a list of all expenses by user id.
+
+	
+	//  write a dynamic query to calculate the net balance between two users.
 }
